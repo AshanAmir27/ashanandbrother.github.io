edaed3af0561da8f5ba3cd9f2e64a94d199d9bf2
Ashan Amir <115730046+AshanAmir27@users.noreply.github.com>
2023-06-02 10:58:55 -0700

    My Construction Website

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="A&Brotherss.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/ea542c129b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Ashan&Brothers.com</title>
+</head>
+
+<body>
+
+
+    <!------------------------Navbar Section------------------------>
+
+    <nav>
+        <label for="" class="logo">Ashan<span style="color:white">&</span>Brother's</label>
+
+        <ul>
+            <li><a class="active" href="#Home">Home</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Services">Services</a></li>
+            <li><a href="#Projects">Projects</a></li>        
+            <li><a href="#review">Reviews</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+
+    <!-------------------------Home Section----------------------->
+
+    <section id="Home">
+
+        <h1 style="position:absolute; top: 30%; left: 25%;">WE PROVIDE <br><span style="color:black">BEST</span> <span
+                style="color:#f74c02"> QUALITY!</span></h1>
+        <p style="position:absolute; top: 48%; left: 25%;">Lorem, ipsum dolor sit amet consectetur <br> adipisicing
+            elit. Sed, tenetur.</p>
+        <a href="/SignUps/index.php" style="position:absolute; top: 57%; left: 25%;">Sign up!</a>
+        <a href="Login.php" style="position:absolute; top: 57%; left: 33%;">Login!</a>
+
+    </section>
+
+    <!-------------------------About Section---------------------->
+
+    <section class="about" id="About">
+        <h1 class="heading">About us</h1>
+        <div class="row">
+
+            <div class="video">
+                <video src="work video.mp4" loop muted autoplay></video>
+            </div>
+            <div class="content">
+                <h3>We Will Provide You The Best<br> Work Which You Dreamt For!</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.Minima veritatis sunt enim dolorum
+                    voluptatem tempora placeat nulla officiis ipsam minus. Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Consequatur rerum... </p><br>
+                <a href="#" class="btn">Read More!</a>
+            </div>
+        </div>
+        <div class="block">
+            <div class="box">
+                <h1>
+                    <div id="value">26+</div>
+                </h1>
+                <p>Years of Experience</p>
+            </div>
+            <div class="box">
+                <h1>
+                    <div id="value">1500+</div>
+                </h1>
+                <p>Projects Completed!</p>
+            </div>
+            <div class="box">
+                <h1>
+                    <div id="value">750+</div>
+                </h1>
+                <p>Satisfied Clients</p>
+            </div>
+
+            <div class="box">
+                <h1>
+                    <div id="value">450+</div>
+                </h1>
+                <p>Active Workers</p>
+            </div>
+        </div>
+    </section>
+
+    <!----------------------Services Section---------------------->
+
+    <section class="services" id="Services">
+        <h1 class="heading">Our Services</h1>
+        <div class="block">
+            <div class="box">
+                <div class="icon">
+                    <i class="fa-solid fa-building" style="color: #f74c02; font-size: 50px;"></i>
+                </div>
+                <h1 class="heading-clr">Building Construction</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            </div>
+            <div class="box boxs">
+                <div class="icon">
+                    <i class="fa-sharp fa-solid fa-screwdriver-wrench" style="color: #f74c02; font-size: 50px;"></i>
+                </div>
+                <h1 class="heading-clr">House Renovation</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+            </div>
+            <div class="box boxs">
+                <div class="icon">
+                    <i class="fa-solid fa-compass-drafting" style="color: #f74c02; font-size: 50px;"></i>
+                </div>
+                <h1 class="heading-clr">Architecture Design</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </div>
+
+            <div class="box boxs">
+                <div class="icon">
+                    <i class="fa-solid fa-truck-pickup" style="color: #f74c02; font-size: 50px;"></i>
+                </div>
+                <h1 class="heading-clr">Material Supply</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </div>
+        </div>
+        <div class="block">
+            <div class="box">
+                <div class="icon">
+                    <i class="fa-solid fa-truck-pickup" style="color: #f74c02; font-size: 50px;"></i>
+                </div>
+                <h1 class="heading-clr">Contruction Consultant</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            </div>
+            <div class="box boxs">
+                <div class="icon">
+                    <i class="fa-sharp fa-solid fa-hammer" style="color: #f74c02; font-size: 50px;"></i>
+                </div>
+                <h1 class="heading-clr">Interior Design</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </div>
+            <div class="box boxs">
+                <div class="icon">
+                    <i class="fa-solid fa-toolbox" style="color: #f74c02; font-size: 50px;"></i>
+                </div>
+                <h1 class="heading-clr">Building Maintenance</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </div>
+
+            <div class="box boxs">
+                <div class="icon">
+                    <i class="fa-solid fa-helmet-safety" style="color: #f74c02; font-size: 50px;"></i>
+                </div>
+                <h1 class="heading-clr">Scaff Folding</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+            </div>
+        </div>
+    </section>
+
+    <!----------------Project Section-------------------->
+
+    <section class="Projects" id="Projects">
+        <h1 class="project_heading">Our Projects</h1>
+
+        <div class="box ">
+            <img src="Project Image 2.jpg" alt="">
+        </div>
+        <div class="box boxp">
+            <img src="Project Image 2.jpg" alt="">
+        </div>
+        <div class="box boxp">
+            <img src="Project Image 3.jpg" alt="">
+        </div>
+        <div class="box boxp">
+            <img src="Project Image 4.jpg" alt="">
+        </div>
+        <div class="box ">
+            <img src="Project Image 5.jpg" alt="">
+        </div>
+        <div class="box boxp">
+            <img src="Project Image 2.jpg" alt="">
+        </div>
+        <div class="box boxp">
+            <img src="Project Image 3.jpg" alt="">
+        </div>
+        <div class="box boxp">
+            <img src="Project Image 5.jpg" alt="">
+        </div>
+
+    </section>
+
+    <!----------------- Reviews----------------->
+
+    
+    <section class="rev-section" id="review">
+
+        <h2 class="title">Reviews</h2>
+
+        <div class="reviews">
+
+            <div class="review">
+                <div class="head-review">
+                    <img src="https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814049__480.png" width="250px">
+                </div>
+                <div class="body-review">
+                    <div class="name-review">Suleman Ahmad</div>
+                    <div class="place-review">Pakistan</div>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </div>
+                    <div class="desc-review">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati
+                        eligendi suscipit illum officia ex eos.</div>
+                </div>
+            </div>
+            <div class="review">
+                <div class="head-review">
+                    <img src="https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814049__480.png" width="250px">
+                </div>
+                <div class="body-review">
+                    <div class="name-review">Ashan Amir</div>
+                    <div class="place-review">England</div>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <div class="desc-review">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati
+                        eligendi suscipit illum officia ex eos.</div>
+                </div>
+            </div>
+            <div class="review">
+                <div class="head-review">
+                    <img src="https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814049__480.png" width="250px">
+                </div>
+                <div class="body-review">
+                    <div class="name-review">Tanzeel Haroon</div>
+                    <div class="place-review">Russia</div>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </div>
+                    <div class="desc-review">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati
+                        eligendi suscipit illum officia ex eos.</div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+     <!---------Contact------------->
+     <section class="contact" id="contact">
+        <h2 class="text-centre">Contact Us</h2>
+        <div class="form">
+            <input class="form-input" type="text" name="name" id="name" placeholder="Enter your Name">
+            <input class="form-input" type="text" name="Email" id="Email" placeholder="Enter your Email">
+            <input class="form-input" type="text" name="Phone" id="Phone" placeholder="Enter your Phone">
+            <textarea class="form-input" name="text" id="text" cols="30" rows="10"
+                placeholder="Elaborate your concern"></textarea>
+            <button class="btn btn-sm btn-dark">Submit</button>
+        </div>
+    </section>
+    <!-----------Contact------------>
+
+     <!------------Footer-------------->
+
+     <footer>
+
+        <p class="text-footer background">
+            Copyright &copy; 2027 www.Ashan&Brothers.com - All rights reserved
+        </p>
+    </footer>
+    <!------------Footer------------>
+
+
+    <script>
+        function animateValue(obj, start, end, duration) {
+            let startTimestamp = null;
+            const step = (timestamp) => {
+                if (!startTimestamp) startTimestamp = timestamp;
+                const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+                obj.innerHTML = Math.floor(progress * (end - start) + start);
+                if (progress < 1) {
+                    window.requestAnimationFrame(step);
+                }
+            };
+            window.requestAnimationFrame(step);
+        }
+
+        const obj = document.getElementById("value");
+        animateValue(obj, 0, 26, 1000);
+    </script>
+
+
+</body>
+
+</html>
